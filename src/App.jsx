c50794c0f084b452e199bc2352fb12976da35673
mhdky <83162131+mhdky@users.noreply.github.com>
2023-05-28 08:18:23 +0700

    props text and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
     <div className="bg-slate-900 w-full h-screen flex justify-center items-center gap-2">
-      <MyButton text="Login" />
+      <MyButton>
+        <i className="fa-brands fa-twitter"></i>
+        Login
+      </MyButton>
       <MyButton text="Register" />
     </div>
   );
@@ -10,6 +13,6 @@ export default function App() {
 // components
 function MyButton(props) {
   return (
-    <button className="bg-blue-600 w-max px-4 py-2 text-white rounded">{props.text}</button>
+    <button className="bg-blue-600 w-max px-4 py-2 flex items-center gap-x-2 text-white rounded">{props.text || props.children}</button>
   )
 }
