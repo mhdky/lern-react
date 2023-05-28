33ffb0b999ca08c06832921d030ee90011e6e61d
mhdky <83162131+mhdky@users.noreply.github.com>
2023-05-28 07:34:59 +0700

    component props.text self closing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <div className="bg-blue-600 w-max mt-[50vh] mx-auto px-4 py-2 text-white rounded">Hello World!</div>
+  return (
+    <div className="bg-slate-900 w-full h-screen flex justify-center items-center gap-2">
+      <MyButton text="Login" />
+      <MyButton text="Register" />
+    </div>
+  );
+}
+
+// components
+function MyButton(props) {
+  return (
+    <button className="bg-blue-600 w-max px-4 py-2 text-white rounded">{props.text}</button>
+  )
 }
