ab96256e314e6f3675a77e17376f16f52741463b
mhdky <83162131+mhdky@users.noreply.github.com>
2023-05-28 08:50:23 +0700

    clsx, dan memberi warna default dan berbeda pada button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 export default function App() {
   return (
     <div className="bg-slate-900 w-full h-screen flex justify-center items-center gap-2">
@@ -5,17 +7,21 @@ export default function App() {
         <i className="fa-brands fa-twitter"></i>
         Login
       </MyButton>
-      <MyButton text="Register" />
+      <MyButton className="bg-pink-600">Register</MyButton>
     </div>
   );
 }
 
 // components
 function MyButton(props) {
-  const {text, children} = props;
+  const {text, children, className = 'bg-blue-600'} = props;
   return (
     // ...props = memanggil semua properti bawaan dari button tingga masukan saja properti yang ingin digunakan pada <MyButton> yang ada di atas
-    <button {...props} className="bg-blue-600 w-max px-4 py-2 flex items-center gap-x-2 text-white rounded">
+    <button {...props} className={clsx (
+                                    className,
+                                    'w-max px-4 py-2 flex items-center gap-x-2 text-white rounded'
+                                  )
+    }>
       {text || children}
     </button>
   )
