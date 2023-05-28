1e7da6b0124cd7ad6b910543accd5e9cfe8befed
mhdky <83162131+mhdky@users.noreply.github.com>
2023-05-28 11:15:01 +0700

    components di lain file

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import clsx from 'clsx';
+import Button from './components/Button';
 
 function App() {
     return (
         <div className='bg-slate-900 w-full h-screen flex justify-center items-center gap-2'>
-            <MyButton type='submit' onClick={() => console.log('login')}>
+            <Button type='submit' onClick={() => console.log('login')}>
                 <i className='fa-brands fa-twitter'></i>
                 Login
-            </MyButton>
-            <MyButton className='bg-pink-600'>Register</MyButton>
+            </Button>
+            <Button className='bg-pink-600'>Register</Button>
         </div>
     );
 }
 
-// components
-function MyButton(props) {
-    const { text, children, className = 'bg-blue-600' } = props;
-    return (
-        // ...props = memanggil semua properti bawaan dari button tingga masukan saja properti yang ingin digunakan pada <MyButton> yang ada di atas
-        <button
-            {...props}
-            className={clsx(
-                className,
-                'w-max px-4 py-2 flex items-center gap-x-2 text-white rounded'
-            )}
-        >
-            {text || children}
-        </button>
-    );
-}
+// // components
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+
+export default function Button(props) {
+    const { children, className = 'bg-blue-600' } = props;
+
+    return (
+        <button
+            {...props}
+            className={clsx(className, 'w-max px-4 py-2 flex items-center gap-2 text-white rounded')}
+        >
+            {children}
+        </button>
+    );
+}
